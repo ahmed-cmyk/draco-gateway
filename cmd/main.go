86c3218fc65f33a0e0d4fff8412815044438aa30
Ahmed Ikram <ikramahmed398@gmail.com>
2026-03-14 20:59:17 +0500

    feat: added basic proxy implementation for testing

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/ahmed-cmyk/GopherGate/internal/config"
 )
@@ -25,10 +28,24 @@ func main() {
 		os.Exit(1)
 	}
 
+	remote, err := url.Parse("https://api.chucknorris.io")
+	if err != nil {
+		panic(err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+
+	port := fmt.Sprintf(":%s", config.Server.Port)
+	srv := &http.Server{
+		Addr:         port,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		Handler:      proxy,
+	}
+
 	// Run server inside a goroutine so that it doesn't block
 	go func() {
-		port := fmt.Sprintf(":%s", config.Server.Port)
-		err := http.ListenAndServe(port, nil)
+		err := srv.ListenAndServe()
 		if err != nil {
 			log.Fatalf("Failed to start server: %v\n", err)
 		}
